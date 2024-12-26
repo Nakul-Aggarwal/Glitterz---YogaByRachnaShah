75a3d9649f6d8619a469a7eff9211dd34a613c77
Nakul Aggarwal <nakulaggarwal98@gmail.com>
2024-12-27 04:11:30 +0530

    Removing i18n from exports

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
